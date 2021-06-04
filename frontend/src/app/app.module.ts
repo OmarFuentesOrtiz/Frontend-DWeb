42490c6bb594291b08563e8aa896ad000787018d
u20181a993 <2bomarfuentesortiz@gmail.com>
2021-06-04 10:42:13 -0500

    Se agrego interface-servicio

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServicioComponent } from './components/servicio/servicio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ServicioComponent } from './components/servicio/servicio.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/interface/interface-servicio.ts
@@ -0,0 +1,12 @@
+export interface IServicio{
+        id: number,
+        modalidad: string,
+        region: string,
+        plataforma: string,
+        usuario: string,
+        name: string,
+        description: string,
+        init_date: string,
+        end_date: string,
+        price: number 
+};
